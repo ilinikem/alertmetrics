5e0fdfc1c6e79b25836d3f246173c29c1a3cd29d
Maksim Sekletov <ti-jey89@mail.ru>
2024-10-05 13:47:02 +0700

    Done inc5

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,6 +1,11 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 // Для адреса и порта
 var flagRunHostAddr string
@@ -12,4 +17,29 @@ func parseFlags() {
 	flag.IntVar(&flagSendFreq, "r", 10, "set frequency for send metrics in seconds")
 	flag.IntVar(&flagGetFreq, "p", 2, "set frequency for get metrics in seconds")
 	flag.Parse()
+
+	// Извлекаю переменную ADDRESS из окружения
+	address := os.Getenv("ADDRESS")
+
+	if address != "" {
+		flagRunHostAddr = address
+	}
+	// Извлекаю переменную REPORT_INTERVAL из окружения
+	repInt := os.Getenv("REPORT_INTERVAL")
+	if repInt != "" {
+		val, err := strconv.Atoi(repInt)
+		if err != nil {
+			fmt.Printf("Error parsing %s: %v\n", repInt, err)
+		}
+		flagSendFreq = val
+	}
+	// Извлекаю переменную POLL_INTERVAL из окружения
+	polInt := os.Getenv("POLL_INTERVAL")
+	if polInt != "" {
+		val, err := strconv.Atoi(polInt)
+		if err != nil {
+			fmt.Printf("Error parsing %s: %v\n", polInt, err)
+		}
+		flagGetFreq = val
+	}
 }

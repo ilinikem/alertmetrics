7f5738e8136460249fdd28acc99a4c30ce6d4b3e
Maksim Sekletov <ti-jey89@mail.ru>
2024-10-05 12:14:01 +0700

    Done inc4

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -8,8 +8,10 @@ var flagSendFreq int
 var flagGetFreq int
 
 func parseFlags() {
+
 	flag.StringVar(&flagRunHostAddr, "a", "localhost:8080", "address and port to run server")
 	flag.IntVar(&flagSendFreq, "r", 10, "set frequency for send metrics in seconds")
 	flag.IntVar(&flagGetFreq, "p", 2, "set frequency for get metrics in seconds")
+
 	flag.Parse()
 }
